4ec7aaf29ab17447f2be0fa16d5ece6103e22844
Hat Dao <enjoy3013@gmail.com>
2014-12-09 20:13:11 +0000

    Fix deploy.php

--- deploy.php
+++ deploy.php
@@ -4,9 +4,11 @@
     echo exec("pwd");
     $listCommand = array(
         "cd /usr/share/nginx/freelancer",
+	'eval "$(ssh-agent -s)"',
+	'ssh-add ~/.ssh/enjoy',
         "git fetch --all",
         "git reset --hard origin/master",
-        "chown -R nginx:nginx ."
+        "chown -R apache:apache ."
     );
     
     // run command
