3549c5a45d35ec2c89ae1e7d905d3e4dd107b3c4
Hat Dao <enjoy3013@gmail.com>
2017-01-12 12:11:28 +0800

    update test

--- test.php
+++ test.php
@@ -1,28 +1,27 @@
 <?php
 //curl -X POST --data-urlencode 'payload={"channel": "#development-team", "username": "webhookbot", "text": "This is posted to #development-team and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
 
-if ($_POST) {
-    $message = "I've just deployed to `heroku` @julianna.j"
-    $message = urlencode($message); // Contains things like 'This is the message to the channel:\n\nHere is the second line with some *bold text* hopefully!'
 
-    $channel = '#development-team';
-    $data = 'payload=' . json_encode(array(
-            'channel'  => $channel,
-            'text'     => $message,
-    ));
+$message = "I've just deployed to `heroku` @julianna.j"
+$message = urlencode($message); // Contains things like 'This is the message to the channel:\n\nHere is the second line with some *bold text* hopefully!'
 
-    $url = 'https://hooks.slack.com/services/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';
+$channel = '#development-team';
+$data = 'payload=' . json_encode(array(
+        'channel'  => $channel,
+        'text'     => $message,
+));
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    if ($result === false) {
-        echo 'Curl error: ' . curl_error($ch);
-    }
+$url = 'https://hooks.slack.com/services/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';
 
-    curl_close($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+if ($result === false) {
+    echo 'Curl error: ' . curl_error($ch);
 }
+
+curl_close($ch);
